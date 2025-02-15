ab5dd63ef83e4f0e7e25065f7d501af3b8020595
Pankaj-Singh486 <pankajsinghamt@gmail.com>
2025-02-15 16:53:24 +0530

    created Navbar and form Handling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/from/form";
+import Nav from "./components/navbar/navBar";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/from/form.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [formData, setFormData] = useState({email: "",password: "", checked: false,});
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted:", formData);
+    alert("Form submitted successfully!");
+  };
+
+  return (
+    <form className="p-4 border border-dark m-4" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="check"
+          name="checked"
+          checked={formData.checked}
+          onChange={handleChange}
+        />
+        <label className="form-check-label" htmlFor="check">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
